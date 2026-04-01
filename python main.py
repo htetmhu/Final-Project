dccdf67b920f7668f192445a7fbd92596f6ca8b9
htetmhu <htety3364@gmail.com>
2026-04-01 18:10:23 +0100

    Starting the codes

---
+++ python main.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+#(GeeksforGeeks, 2017)
+#Initializing the window 
+root = tk.Tk()
+root.title("Student Progress Tracker")
+root.geometry("400x300")   #Setting the size of window
+
+root.mainloop()   #Output GUI

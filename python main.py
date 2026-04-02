81a4c2889f904a9623713e7e14eb43d49dadf2f3
htetmhu <htety3364@gmail.com>
2026-04-02 16:06:58 +0100

    Adding more subjects and updating UI

--- python main.py
+++ python main.py
@@ -4,41 +4,36 @@ import tkinter as tk
 #(www.w3schools.com, n.d.)
 students_lists = {}
 
-def adding_student_info():
-    #Getting the information from entry
-    #(GeeksforGeeks, 2020)
-    name = entry_name.get().strip()
-    subject = entry_name.get().strip()
+def adding_info_grades():
+    #(W3Schools, n.d.)
+    #adding id and name using strip so that it removes the whitespaces 
+    id_update = id_entry.get().strip()
+    name_update = entry_name.get().strip()
     
-    #(W3 Schools, 2024)
-    #Coding for the validation input, not to crash so writing the code with try/except
     try:
-        grade = float(entry_grade.get())
+        math_grade = float(entry_math.get())
+        sci_grade = float(entry_sci.get())
+        it_grade = float(entry_it.get())
         
         #(GeeksforGeeks, 2018)
-        #Creating a nested dictionary 
-        if name not in students_lists:
-            students_lists[name] = {}
+        #Using id as primary key to access
+        students_lists[id_update] = {
+            "Name": name_update,
+            "Grades": {"Math": math_grade, "Science": sci_grade, "IT": it_grade}
+        }
         
-        #Adding the subject to each student like Math, Science, IT --> Student 
-        students_lists[name][subject] = grade
+        # Average Calculation
+        average = (math_grade + sci_grade + it_grade) / 3
         
-        #(www.w3schools.com, n.d.)
-        #Calculating the averages of dictionary values 
-        all_grades = students_lists[name].values()
-        avg = sum(all_grades) / len(all_grades)
-        
-        print(f"Updated {name}: Average is {avg:.2f}")   #Using f string to print 
-        
-        #(www.w3schools.com, n.d.)
-        #With text file, saving data permanently so that they will not deleted 
-        with open("student_info_grades.txt", "w") as f:
-            for name, data in students_lists.items():
-                for subject, grade in data.items():
-                    f.write(f"{name},{subject},{grade}\n")
+        print(f"Success: ID {id_update} ({name_update}) added with average {average:.2f}")  #using f string to print
+
+        #(W3Schools, 2019)
+        #Creating and adding the input to text file 
+        with open("student_data.txt", "a") as file:
+            file.write(f"ID:{id_update}, Name:{name_update}, Math:{math_grade}, Science:{sci_grade}, IT:{it_grade}, Average:{average:.2f}\n")
 
     except ValueError:
-        print("Error: Please enter a valid number for the grade.")
+        print("Error: Ensure ID is entered and grades are numeric!")
 
 
 #(GeeksforGeeks, 2017)
@@ -48,26 +43,30 @@ root.title("Student Progress Tracker")
 root.geometry("400x300")   #Setting the size of window
 
 #(GeeksforGeeks, 2019)
-#Using labels
-tk.Label(root, text="Name of Student:").grid(row=0, column=0, padx=10, pady=5)
+#Creating the entry using labels
+tk.Label(root, text="Student ID:").grid(row=0, column=0)
+id_entry = tk.Entry(root)
+id_entry.grid(row=0, column=1)
+
+tk.Label(root, text="Name of Student:").grid(row=1, column=0, padx=10, pady=5)
 entry_name = tk.Entry(root)
-entry_name.grid(row=0, column=1)
+entry_name.grid(row=1, column=1)
 
-tk.Label(root, text="Student's Subject:").grid(row=1, column=0, padx=10, pady=5)
-entry_subj = tk.Entry(root)
-entry_subj.grid(row=1, column=1)
+tk.Label(root, text="Mathematics Grade:").grid(row=2, column=0)
+entry_math = tk.Entry(root)
+entry_math.grid(row=2, column=1)
 
-tk.Label(root, text="Grade:").grid(row=2, column=0, padx=10, pady=5)
-entry_grade = tk.Entry(root)
-entry_grade.grid(row=2, column=1)
+tk.Label(root, text="Science Grade:").grid(row=3, column=0)
+entry_sci = tk.Entry(root)
+entry_sci.grid(row=3, column=1)
+
+tk.Label(root, text="IT Grade:").grid(row=4, column=0)
+entry_it = tk.Entry(root)
+entry_it.grid(row=4, column=1)
 
 #(GeeksforGeeks, 2019)
-#Creating buttons to access
-button_submit = tk.Button(root, text="Add Data", width=15)
-button_submit.grid(row=3, column=1, pady=10)
-
 #Linking the function of add_student_data with button using command 
-submit_btn = tk.Button(root, text="Add Data", width=15, command=adding_student_info)
-submit_btn.grid(row=3, column=1, pady=10)
+submit_btn = tk.Button(root, text="Add Data", width=15, command=adding_info_grades)
+submit_btn.grid(row= 5, column=1, pady=10)
 
 root.mainloop()   #Output GUI
